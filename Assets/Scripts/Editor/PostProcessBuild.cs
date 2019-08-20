1bc90b9ef4bc5d14097e9673c35742d785074ac9
renardchien <renardchien@gmail.com>
2019-08-20 15:59:12 -0400

    added build scripts

---
+++ Assets/Scripts/Editor/PostProcessBuild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace ARPeerToPeerSample.Editor
+{
+    public static class PostProcessBuild
+    {
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+        {
+            if (buildTarget == BuildTarget.iOS)
+            {
+                string libraryPath = buildPath + "/Libraries/";
+                string libraryMultipeerSourcePath = libraryPath + "/Addons/UnityARKitMultipeerConnectivity/UnityMultipeerConnectivity/";
+                string libraryUnitySwiftSourcePath = libraryPath + "/Addons/UnityARKitMultipeerConnectivity/UnitySwift/";
+                string multipeerDestPath = libraryPath + "UnityMultipeerConnectivity/";
+                string swiftDestPath = libraryPath + "UnitySwift/";
+
+                try
+                {
+                    //FileUtil.MoveFileOrDirectory(libraryMultipeerSourcePath, multipeerDestPath);
+                    FileUtil.MoveFileOrDirectory(libraryUnitySwiftSourcePath, swiftDestPath);
+                }
+                catch(Exception e)
+                {
+                    Debug.Log("Failed to write file: " + e.ToString());
+                }
+            }
+        }
+    }
+}
